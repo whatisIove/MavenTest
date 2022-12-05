c75676b07eadc798ab6deb8f4fe503aaccde74ed
whatisIove <vladwfop@gmail.com>
2022-12-05 14:51:41 +0200

    Maven Project test.

--- src/main/java/ua/ithillel/homework20/Client.java
+++ src/main/java/ua/ithillel/homework20/Client.java
@@ -1,2 +1,21 @@
-package ua.ithillel.homework20;public class Client {
+package ua.ithillel.homework20;
+
+import lombok.*;
+
+@Setter
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+@ToString
+
+public class Client {
+    private long id;
+    private String clientName;
+    private double balance;
+
+    public Client(long id, String clientName, double balance) {
+        this.id = id;
+        this.clientName = clientName;
+        this.balance = balance;
+    }
 }
--- src/main/java/ua/ithillel/homework20/Main.java
+++ src/main/java/ua/ithillel/homework20/Main.java
@@ -1,5 +1,43 @@
-package ua.ithillel.homework20;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package ua.ithillel.homework20;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.*;
+import java.net.URL;
+import java.util.List;
+
+public class Main {
+
+
+    public Main() throws IOException {
     }
+
+    public static void main(String[] args) throws IOException {
+
+        final Client client = new Client(445, "Test", 20.25);
+        final Client user = new Client();
+        user.setId(445);
+        user.setClientName("Test");
+        user.setBalance(20.25);
+        System.out.println(client.equals(user));
+
+        try (InputStream in = new URL("https://commons.apache.org").openStream()) {
+            InputStreamReader inR = new InputStreamReader(in);
+            BufferedReader buf = new BufferedReader(inR);
+            String line;
+            while ((line = buf.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    File file = new File("/commons/io/project.properties");
+    List<String> lines = FileUtils.readLines(file, "UTF-8");
+
+    String filename = "C:/commons/io/../lang/project.xml";
+    String normalized = FilenameUtils.normalize(filename);
+    // result is "C:/commons/lang/project.xml"
 }
